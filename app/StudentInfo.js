af073daf79d9a1f104107c5245a3cc65e4f6bdec
Nathan Chan <123600671+nathanc726@users.noreply.github.com>
2023-09-21 09:01:36 -0600

    first attempt

---
+++ app/StudentInfo.js
@@ -0,0 +1,9 @@
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Name: Ho Pong Chan (Nathan)</h1>
+            <h2>Course section: CPRG 306 B</h2>
+            <p>https://github.com/nathanc726</p>
+        </div>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import StudentInfo from '../StudentInfo';
+
+export default function Page() {
+    return (
+        <main>
+            <h1>My Shopping List</h1>
+            <StudentInfo/>
+        </main>
+    );
+}
