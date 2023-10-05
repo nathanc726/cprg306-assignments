bbdf0b45a4d3611e9b4f63a8f6fd77744966b1e1
Nathan Chan <123600671+nathanc726@users.noreply.github.com>
2023-10-05 04:16:50 -0600

    Assignment 4
    week 4 New-item

--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
       <div class="block m-8 ml-16" > 
         <div><Link href="/week2">Week 2</Link></div>
         <div><Link href="/week3">Week 3</Link></div>
+        <div><Link href="/week4">Week 4</Link></div>
       </div>
     </main>
   );
